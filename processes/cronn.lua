284bf6dac393b5b9e6109ee11aaa78d5cb4e75b9
Ashutosh Mittal <132162510+AAshu1412@users.noreply.github.com>
2025-09-04 19:14:03 +0530

    Update cronn.lua

--- processes/cronn.lua
+++ processes/cronn.lua
@@ -4,7 +4,9 @@ Handlers.add(
   "CronTick", -- handler name
   Handlers.utils.hasMatchingTag("Action", "Cron"), -- handler pattern to identify cron message
   function (msg) -- handler task to execute on cron message
-   
+   print("CHECK START")
    Send({Target="SpJdYt1_CdMG0W5JjDVYfu-tJNGB2bXC6usBSyrQAys", Action="cronnn"})
+   Send({Target="SpJdYt1_CdMG0W5JjDVYfu-tJNGB2bXC6usBSyrQAys", Action="Auto-Token-Pair-Yield-Optimizer"})
+    print("CHECK END")
   end
 )
